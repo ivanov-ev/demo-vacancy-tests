f6fc631b35dc1b0a325e04dc80522337ea7a8a1a
Eugene Ivanov <email.eugene-ivanov@yandex.ru>
2024-06-08 13:38:36 +0300

    Fix WebDriver params

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -2,22 +2,28 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources(
-        {"classpath:${env}.properties"}
-)
+@Config.Sources({"classpath:${env}.properties"})
 public interface WebDriverConfig extends Config {
 
+    @Key("selenide.browser")
     @DefaultValue("chrome")
     String browser();
 
+    @Key("selenide.browserVersion")
     String browserVersion();
 
+    @Key("selenide.browserSize")
     @DefaultValue("1920x1000")
     String browserSize();
 
+    @Key("selenide.remote")
     String remote();
 
+    @Key("selenide.user")
+    @DefaultValue("user1")
     String remoteUsername();
 
+    @Key("selenide.password")
+    @DefaultValue("1234")
     String remotePassword();
 }
