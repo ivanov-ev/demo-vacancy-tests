7d17fb1871705b3ef33790c8174f5fce34c088b1
Eugene Ivanov <email.eugene-ivanov@yandex.ru>
2024-06-08 12:37:36 +0300

    Revert "Move WebDriver initiation from TestBase.java to WebDriverProvider.java"
    This reverts commit 473d073551e91839e1ad742da9f0f4e3fe55ae23.

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,14 @@
 package tests;
 
-import config.WebDriverProvider;
+import com.codeborne.selenide.Configuration;
+import config.WebDriverConfig;
 import helpers.Attach;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -11,20 +16,47 @@ public class TestBase {
 
     @BeforeAll
     static void setup() {
-        WebDriverProvider.initConfiguration();
-    }
 
-    @AfterEach
-    void tearDown() {
-        addAttachments();
-        closeWebDriver();
-    }
+        WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
 
+        Configuration.browser = webDriverConfig.browser();
+        Configuration.browserVersion = webDriverConfig.browserVersion();
+        Configuration.browserSize = webDriverConfig.browserSize();
+
+        //pay attention to the trailing slash, because it concatenates with open() and may result in 2 slashes
+        Configuration.baseUrl = "https://hflabs.ru";
+
+        Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = webDriverConfig.remote();
+        if (Configuration.remote != null) {
+            Configuration.remote = "https://"
+                    + webDriverConfig.remoteUsername() + ":"
+                    + webDriverConfig.remotePassword() + "@"
+                    + Configuration.remote
+                    + "/wd/hub";
+        }
+
+        Configuration.webdriverLogsEnabled = true;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo",true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
 
     void addAttachments() {
         Attach.screenshotAs("Screenshot");
         Attach.addVideo();
         Attach.browserConsoleLogs();
         Attach.pageSource();
+    }
+
+    @AfterEach
+    void tearDown() {
+        addAttachments();
+        closeWebDriver();
     }
 }
